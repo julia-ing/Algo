13557bfaa56228973b26b355944f594a9e995369
Yewon Choi <ywchoi0625@gmail.com>
2024-01-21 00:18:45 +0900

    [BOJ] 수 묶기: 구현

--- BOJ/1744_수묶기.py
+++ BOJ/1744_수묶기.py
@@ -12,11 +12,12 @@ for n in nums:
         pos.append(n)
     elif n <= 0:
         neg.append(n)
-        
+
+# 양수는 내림차순 정렬해서 큰 수들부터 곱하기
 pos.sort(reverse=True)
+# 음수와 0은 작은 값들부터 곱하기
 neg.sort()
 
-# 양수면 곱하는게 이득
 while pos:
     if len(pos) == 1:
         res += pos.pop(0)
